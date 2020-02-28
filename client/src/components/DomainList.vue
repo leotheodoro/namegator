544bf4e233f2361bdc4996e0c710cec757bc9320
Leonardo Theodoro <2038128-theodoro.dev@users.noreply.gitlab.com>
2020-02-28 10:43:56 -0300

    graphql api back e front end

--- client/src/components/DomainList.vue
+++ client/src/components/DomainList.vue
@@ -43,6 +43,7 @@
 import "bootstrap/dist/css/bootstrap.css"
 import "font-awesome/css/font-awesome.css"
 import AppItemList from "./AppItemList";
+import axios from "axios/dist/axios";
 
 export default {
   name: 'App',
@@ -51,8 +52,8 @@ export default {
   },
   data: function() {
     return {
-      prefixes: ['Air', 'Jet', 'Flight'],
-      sufixes: ['Hub', 'Station', 'Mart'],
+      prefixes: [],
+      sufixes: [],
     }
   },
   methods: {
@@ -86,6 +87,34 @@ export default {
 
       return domains;
     }
+  },
+  created() {
+    axios({
+      url: 'http://localhost:4000',
+      method: 'POST',
+      data: {
+        query: `
+          {
+            prefixes: items(type: "prefix"){
+              id
+              type
+              description
+            }
+
+            sufixes: items(type: "sufix"){
+              id
+              type
+              description
+            }
+          }
+        `
+      }
+    }).then(res => {
+      const query = res.data;
+      this.prefixes = query.data.prefixes.map(prefix => prefix.description);
+      this.sufixes = query.data.sufixes.map(sufix => sufix.description);
+      console.log(query.data);
+    });
   }
 }
 </script>
